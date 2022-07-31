ddf390e7f157996e5627dc6ab7b4cbbcd1d0e046
Md. Sumon Mia <md.sumonmia.cse.iu@gmail.com>
2022-08-01 02:28:52 +0600

    3 solved added

---
+++ pblm-1.py
@@ -0,0 +1,7 @@
+from cgitb import reset
+
+
+n = int(input('Enter a inter number:'))
+f = float(input('Enter a float number:'))
+reset = n*f
+print("Your result is:",reset)
---
+++ pblm-2.py
@@ -0,0 +1,10 @@
+
+# first = 12
+# second = 6
+# result = first**second
+# print('Your result:', result)
+
+base = int(input('Enter base number:'))
+power = int(input('Enter pow number:'))
+result = pow(base, power)
+print(result)
